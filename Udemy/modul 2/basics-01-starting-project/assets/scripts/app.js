a403bd1d5ecf6f1a684e300d3eb976b3515d82d5
viieee <adhityaharviando@gmail.com>
2020-09-15 18:38:20 +0700

    udemy first

---
+++ Udemy/modul 2/basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,10 @@
+//section 2
+//variable & constant
+//let x = 123; //variabel, penampung data. x adalah nama dari variabel, nilai dari variabel terletak setelah tanda "="
+//const i = 1; //variabel tapi tidak bisa diganti valuenya.
+
+let currentResult; //variabel sudah dideklarasi tapi belum di inisialisasi/didefinisi (belum diberi value)
+currentResult = 0; //mendefinisikan variabel (memberi value)
+
+
+
