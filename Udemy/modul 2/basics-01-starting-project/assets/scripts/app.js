ced4f1c890cbf50020399253d69c9d45f9200f11
viieee <adhityaharviando@gmail.com>
2020-09-16 16:35:29 +0700

    next

--- Udemy/modul 2/basics-01-starting-project/assets/scripts/app.js
+++ Udemy/modul 2/basics-01-starting-project/assets/scripts/app.js
@@ -2,9 +2,11 @@
 //variable & constant
 //let x = 123; //variabel, penampung data. x adalah nama dari variabel, nilai dari variabel terletak setelah tanda "="
 //const i = 1; //variabel tapi tidak bisa diganti valuenya.
+const defaultResult = 0;
+let currentResult = 10; //variabel sudah dideklarasi tapi belum di inisialisasi/didefinisi (belum diberi value)
+//mendefinisikan variabel (memberi value)
 
-let currentResult; //variabel sudah dideklarasi tapi belum di inisialisasi/didefinisi (belum diberi value)
-currentResult = 0; //mendefinisikan variabel (memberi value)
-
-
-
+// string bisa menggunakan '', "", dan ``
+let contohTandaPetikBeda = `(${defaultResult} adalah hasil)`; // pake `` bisa begini, nampilin variabel pake ${}
+outputResult(currentResult, contohTandaPetikBeda);
+console.log(contohTandaPetikBeda);
