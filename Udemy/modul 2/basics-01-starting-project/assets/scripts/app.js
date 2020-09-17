2a41a02e87f3f49f3b9a811820c1c46cc5c2ee6a
viieee <adhityaharviando@gmail.com>
2020-09-17 12:08:26 +0700

    function

--- Udemy/modul 2/basics-01-starting-project/assets/scripts/app.js
+++ Udemy/modul 2/basics-01-starting-project/assets/scripts/app.js
@@ -3,10 +3,16 @@
 //let x = 123; //variabel, penampung data. x adalah nama dari variabel, nilai dari variabel terletak setelah tanda "="
 //const i = 1; //variabel tapi tidak bisa diganti valuenya.
 const defaultResult = 0;
-let currentResult = 10; //variabel sudah dideklarasi tapi belum di inisialisasi/didefinisi (belum diberi value)
+let currentResult = defaultResult; //variabel sudah dideklarasi tapi belum di inisialisasi/didefinisi (belum diberi value)
 //mendefinisikan variabel (memberi value)
 
 // string bisa menggunakan '', "", dan ``
 let contohTandaPetikBeda = `(${defaultResult} adalah hasil)`; // pake `` bisa begini, nampilin variabel pake ${} (template literal)
-outputResult(currentResult, contohTandaPetikBeda);
 console.log(contohTandaPetikBeda);
+
+function hitungTambah(num1, num2) {
+  const hasil = num1 + num2;
+  outputResult(hasil, num2);
+}
+
+hitungTambah(currentResult, 1);
