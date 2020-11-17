0b065e39079c39b04e30dd75de7b399789634787
viieee <adhityaharviando@gmail.com>
2020-11-17 16:19:29 +0700

    adding event listener to the button and simplying the code

--- Udemy/modul 4/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ Udemy/modul 4/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,13 +1,36 @@
-const ATTACK_VALUE = 10;
+const PLAYER_ATTACK_VALUE = 10;
+const PLAYER_STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
 let chosenMaxLife = 100;
-
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
 // set value health bars
 adjustHealthBars(chosenMaxLife);
 
+// some additional functions
+function attackz(damageFromPlayer, damageFromMonster) {
+  const damage = dealMonsterDamage(damageFromPlayer);
+  currentMonsterHealth -= damage;
+  const monsterDamage = dealPlayerDamage(damageFromMonster);
+  currentPlayerHealth -= monsterDamage;
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You Win!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('You Lose!');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("It's a draw!");
+  }
+}
+
 // event listener's functions
 function attackHandler() {
-  dealMonsterDamage(ATTACK_VALUE);
+  attackz(PLAYER_ATTACK_VALUE, MONSTER_ATTACK_VALUE);
+}
+
+function strongAttackHandler() {
+  attackz(PLAYER_STRONG_ATTACK_VALUE, MONSTER_ATTACK_VALUE);
 }
 
 // event listener
 attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
