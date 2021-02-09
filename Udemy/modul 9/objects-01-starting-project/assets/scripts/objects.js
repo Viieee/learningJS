174ce4fcc02999b39d6e3c4ed9e4999663684f5a
viieee <adhityaharviando@gmail.com>
2021-02-10 06:48:09 +0700

    making objects

---
+++ Udemy/modul 9/objects-01-starting-project/assets/scripts/objects.js
@@ -0,0 +1,10 @@
+const person = {
+    aProperty: 'value',
+    name: 'Vieri',
+    age: 20,
+    hobbies: ['gaming', 'coding'],
+    greet: function(){
+        alert('hi! this alert is called from a method');
+    }
+}
+
