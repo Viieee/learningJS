a5e5bfb2fbb37df9a18d22cf1233e02e0220be92
viieee <adhityaharviando@gmail.com>
2021-02-10 18:31:27 +0700

    latihan

--- Udemy/modul 9/objects-01-starting-project/assets/scripts/objects.js
+++ Udemy/modul 9/objects-01-starting-project/assets/scripts/objects.js
@@ -4,6 +4,46 @@ const filterMovieButton = document.getElementById('search-btn');
 
 let movies = [];
 
+const movieList = document.getElementById('movie-list');
+
+// function
+function renderMovies(filter = ''){ // default value
+    
+    if(movies.length===0){
+        movieList.classList.remove('visible')
+        return;
+    }else{
+        movieList.classList.add('visible')
+    }
+    
+    // ternary condition
+    // if the filter is indeed falsy/empty then all the movies will be shown
+    // if the filter is truthy then use filter()
+    const filteredMovies = !filter ? movies : movies.filter(function(element){
+        return element.info.title.includes(filter); // akan mereturn hasil dari isi array yang mengandung elemen filter
+    }); 
+    
+    movieList.innerHTML=''; // akan mereset value yang ada dibawah movieList di HTML code
+                            // akan memastikan bahwa movieList akan di re-create setiap button dipencet
+    
+    
+    // untuk tiap elemen movie
+    filteredMovies.forEach(function(movie){ //bertanggung jawab untuk membuat element li dan mengisi kedalam movielist
+        const movieElement = document.createElement('li');
+        let text = movie.info.title + ' - '
+        // for in loop for objects, going through all key-value pair in an object
+        for (const key in movie.info){  //untuk set text content
+            if(key !== 'title'){ // mengexclude property title dalam object info
+                text += `${key}: ${movie.info[key]}`; // ${key} will be the key inside info object in newMovie object
+            }                                         // ${movie.info[key]} is the value of the property key
+        }
+        movieElement.textContent = text;
+        movieList.append(movieElement);
+    })
+}
+
+
+// button function
 function addMovieHandler(){
 // user inputs
 const title = document.getElementById('title').value;
@@ -21,8 +61,15 @@ const extraValue = document.getElementById('extra-value').value;
         }, 
         id: Math.random()
     }
-    movies.push(newMovie)
-    console.log(newMovie, movies)
+    movies.push(newMovie);
+    console.log(newMovie, movies);
+    renderMovies();
+}
+
+function searchMovieHandler(){
+    const filterInput = document.getElementById('filter-title').value;
+    renderMovies(filterInput);
 }
 
 addMovieButton.addEventListener('click',addMovieHandler);
+filterMovieButton.addEventListener('click',searchMovieHandler);
