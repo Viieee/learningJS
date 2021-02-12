b1223d9d48bf76e2837de6365a75f98669730d6c
viieee <adhityaharviando@gmail.com>
2021-02-12 13:59:30 +0700

    implementing class

--- Udemy/modul 10/oop-01-starting-setup/assets/scripts/app.js
+++ Udemy/modul 10/oop-01-starting-setup/assets/scripts/app.js
@@ -1,19 +1,55 @@
+class Product { //class harus kapital, class adalah blueprint dari object
+    // fields adalah property dalam class
+    // nama field harus sama kayak object yang lain
+    name = 'Default value';
+    imageUrl;
+    price;
+    desc;
+
+    // berarti object yang based on this class akan memiliki propertie seperti field diatas
+
+    // method inside class
+    // productClassMethod(){}
+
+    // constructor method
+    // mengambil argumen dari luar ketika class dipanggil
+    constructor(name,image,price,desc){
+        //this akan mengarah ke class ini
+        this.name = name;
+        this.imageUrl = image;
+        this.price = price;
+        this.description = desc;
+    }
+}
+
+
+
 const productList = {
     products: [
-        {
-            name: 'Golf le fleur x Converse Gianno Ox Vintage White',
-            image: 'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:700/output=quality:70/compress/https://cdn.fs.grailed.com/api/file/e8J1lYUAQRq9S3x2vgwB',
-            price: 220,
-            description: 'Converse collab with golf le fleur'
-        },
-        {
-            name: 'Golf le fleur x Converse Chuck taylor 70s High White',
-            image: 'https://stockx-360.imgix.net/Converse-Chuck-Taylor-All-Star-70s-Hi-Golf-Le-Fleur-Parchment/Images/Converse-Chuck-Taylor-All-Star-70s-Hi-Golf-Le-Fleur-Parchment/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1607672662&w=1000',
-            price: 200,
-            description: 'Converse collab with golf le fleur'
-        }
+        new Product(
+        'GOLF Le Fleur Lacoste Teddy Jacket Green',
+        'https://images.stockx.com/products/streetwear/GOLF-Le-Fleur-Lacoste-Teddy-Jacket-Green.png?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&trim=color&updated_at=1603481985&w=1000',
+        800,
+        'Golf le fleur collab with lacoste'), //memanggil class
+        new Product(
+        'Golf le fleur x Converse Gianno Ox Vintage White',
+        'https://process.fs.grailed.com/AJdAgnqCST4iPtnUxiGtTz/auto_image/cache=expiry:max/rotate=deg:exif/resize=height:700/output=quality:70/compress/https://cdn.fs.grailed.com/api/file/e8J1lYUAQRq9S3x2vgwB',
+        220,
+        'Converse collab with golf le fleur'), //memanggil class
+        new Product(
+        'Golf le fleur x Converse Chuck taylor 70s High White',
+        'https://stockx-360.imgix.net/Converse-Chuck-Taylor-All-Star-70s-Hi-Golf-Le-Fleur-Parchment/Images/Converse-Chuck-Taylor-All-Star-70s-Hi-Golf-Le-Fleur-Parchment/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1607672662&w=1000',
+        200,
+        'Converse collab with golf le fleur') //memanggil class
+        // sama seperti
+        // {
+        //     name: 'Golf le fleur x Converse Chuck taylor 70s High White',
+        //     imageUrl: 'https://stockx-360.imgix.net/Converse-Chuck-Taylor-All-Star-70s-Hi-Golf-Le-Fleur-Parchment/Images/Converse-Chuck-Taylor-All-Star-70s-Hi-Golf-Le-Fleur-Parchment/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1607672662&w=1000',
+        //     price: 200,
+        //     description: 'Converse collab with golf le fleur'
+        // }
     ],
-    render: function(){
+    render(){
         const renderDiv = document.getElementById('app');
         // create an unordered list
         const ulProdList = document.createElement('ul');
@@ -24,7 +60,7 @@ const productList = {
             liElementProduct.className = 'product-item';
             liElementProduct.innerHTML = `
             <div>
-                <img src="${prod.image}" alt="${prod.name}">
+                <img src="${prod.imageUrl}" alt="${prod.name}">
                 <div class='product-item__content'>
                     <h2>${prod.name}</h2>
                     <h3>\$${prod.price}</h3>
