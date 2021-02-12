64ea710b3a4b290a305548b265cb4f7cd2121ca2
viieee <adhityaharviando@gmail.com>
2021-02-12 16:15:10 +0700

    multiple classes

--- Udemy/modul 10/oop-01-starting-setup/assets/scripts/app.js
+++ Udemy/modul 10/oop-01-starting-setup/assets/scripts/app.js
@@ -1,19 +1,5 @@
-class Product { //class harus kapital, class adalah blueprint dari object
-    // fields adalah property dalam class
-    // nama field harus sama kayak object yang lain
-    // bila class memiliki constructor, tidak perlu mendefine fields
-    // name = 'Default value';
-    // imageUrl;
-    // price;
-    // description;
-
-    // berarti object yang based on this class akan memiliki propertie seperti field diatas
-
-    // method inside class
-    // productClassMethod(){}
-
-    // constructor method
-    // mengambil argumen dari luar ketika class dipanggil
+// containing product data
+class Product {
     constructor(name,image,price,desc){
         //this akan mengarah ke class ini
         this.name = name;
@@ -23,10 +9,8 @@ class Product { //class harus kapital, class adalah blueprint dari object
     }
 }
 
-
-
-const productList = {
-    products: [
+class ProductList{
+    products = [
         new Product(
         'GOLF Le Fleur Lacoste Teddy Jacket Green',
         'https://images.stockx.com/products/streetwear/GOLF-Le-Fleur-Lacoste-Teddy-Jacket-Green.png?fit=fill&bg=FFFFFF&w=700&h=500&auto=format,compress&q=90&trim=color&updated_at=1603481985&w=1000',
@@ -41,39 +25,49 @@ const productList = {
         'Golf le fleur x Converse Chuck taylor 70s High White',
         'https://stockx-360.imgix.net/Converse-Chuck-Taylor-All-Star-70s-Hi-Golf-Le-Fleur-Parchment/Images/Converse-Chuck-Taylor-All-Star-70s-Hi-Golf-Le-Fleur-Parchment/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1607672662&w=1000',
         200,
-        'Converse collab with golf le fleur') //memanggil class
-        // sama seperti
-        // {
-        //     name: 'Golf le fleur x Converse Chuck taylor 70s High White',
-        //     imageUrl: 'https://stockx-360.imgix.net/Converse-Chuck-Taylor-All-Star-70s-Hi-Golf-Le-Fleur-Parchment/Images/Converse-Chuck-Taylor-All-Star-70s-Hi-Golf-Le-Fleur-Parchment/Lv2/img01.jpg?auto=format,compress&q=90&updated_at=1607672662&w=1000',
-        //     price: 200,
-        //     description: 'Converse collab with golf le fleur'
-        // }
-    ],
+        'Converse collab with golf le fleur')
+    ];
+
+    constructor (){}
+
     render(){
         const renderDiv = document.getElementById('app');
         // create an unordered list
         const ulProdList = document.createElement('ul');
         ulProdList.className = 'product-list';
         for(const prod of this.products){ // for each element in products array property
-            // create li
-            const liElementProduct = document.createElement('li');
-            liElementProduct.className = 'product-item';
-            liElementProduct.innerHTML = `
-            <div>
-                <img src="${prod.imageUrl}" alt="${prod.name}">
-                <div class='product-item__content'>
-                    <h2>${prod.name}</h2>
-                    <h3>\$${prod.price}</h3>
-                    <p>${prod.description}</p>
-                    <button>Add to Cart</button>
-                </div>
-            </div>
-            `;
-            ulProdList.append(liElementProduct);
+            const productItem = new ProductItem(prod);
+            const liEl = productItem.render();
+            ulProdList.append(liEl);
         }
         renderDiv.append(ulProdList);
     }
-};
+}
 
+// logic to render per item
+class ProductItem {
+    constructor(product){
+        this.product = product;
+    }
+    render(){
+         // create li
+         const liElementProduct = document.createElement('li');
+         liElementProduct.className = 'product-item';
+         liElementProduct.innerHTML = `
+         <div>
+             <img src="${this.product.imageUrl}" alt="${this.product.name}">
+             <div class='product-item__content'>
+                 <h2>${this.product.name}</h2>
+                 <h3>\$${this.product.price}</h3>
+                 <p>${this.product.description}</p>
+                 <button>Add to Cart</button>
+             </div>
+         </div>
+         `; // 'this' refering to the class, product is the property
+         return liElementProduct;
+    }
+}
+
+
+const productList = new ProductList();
 productList.render();
