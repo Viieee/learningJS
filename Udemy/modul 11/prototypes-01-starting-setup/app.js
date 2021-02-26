4b2752e418678fb40aa1c2d8993681c125e31d40
viieee <adhityaharviando@gmail.com>
2021-02-26 20:47:11 +0700

    constructor

--- Udemy/modul 11/prototypes-01-starting-setup/app.js
+++ Udemy/modul 11/prototypes-01-starting-setup/app.js
@@ -1,9 +1,33 @@
-class Person{
-    name='Vieri';
-    constructor(){
-        this.age=21;
-    }
-    greet(){
+// class Person{
+//     name='Vieri';
+//     constructor(){
+//         this.age=21;
+//     }
+//     greet(){
+//         console.log('Hi, my name is '+ this.name + ', i\'m '+this.age);
+//     }
+// }
+// is similar to :
+// constructor function
+function Person(){
+    this.age = 30;
+    this.name = 'Vieri';
+    this.greet = ()=>{
         console.log('Hi, my name is '+ this.name + ', i\'m '+this.age);
     }
 }
+const prsn = new Person(); 
+// instantiating the function with the new keyword will make an object called 'this'
+// then every properties and method inside the function will be moved into the 'this' object
+// after that  the function will return the 'this' object
+// the code representation of it :
+// function Person(){
+//     this = {age: 30,
+//         name: 'Vieri',
+//         greet: ()=>{
+//             console.log('Hi, my name is '+ this.name + ', i\'m '+this.age);
+//     }}
+//     return this;
+// }
+
+prsn.greet();
