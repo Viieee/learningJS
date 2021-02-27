0f089ba90e8c5bed72f4f9fadfb95636a452c391
viieee <adhityaharviando@gmail.com>
2021-02-27 15:39:27 +0700

    prototype deep dive

--- Udemy/modul 11/prototypes-01-starting-setup/app.js
+++ Udemy/modul 11/prototypes-01-starting-setup/app.js
@@ -1,21 +1,21 @@
-// class Person{
-//     name='Vieri';
-//     constructor(){
-//         this.age=21;
-//     }
-//     greet(){
+ class Person{
+     name='Vieri';
+     constructor(){
+         this.age=21;
+     }
+     greet(){
+         console.log('Hi, my name is '+ this.name + ', i\'m '+this.age);
+     }
+ }
+// is similar to :
+// constructor function
+// function Person(){
+//     this.age = 30;
+//     this.name = 'Vieri';
+//     this.greet = ()=>{
 //         console.log('Hi, my name is '+ this.name + ', i\'m '+this.age);
 //     }
 // }
-// is similar to :
-// constructor function
-function Person(){
-    this.age = 30;
-    this.name = 'Vieri';
-    this.greet = ()=>{
-        console.log('Hi, my name is '+ this.name + ', i\'m '+this.age);
-    }
-}
 const prsn = new Person(); 
 // instantiating the function with the 'new' keyword will make an object called 'this'
 // then every properties and method inside the function will be moved into the 'this' object
@@ -31,4 +31,11 @@ const prsn = new Person();
 // }
 
 prsn.greet();
-
+console.log(prsn)
+console.dir(Person);
+// Hi, my name is Vieri, i'm 21
+// app.js:34 
+// Person {name: "Vieri", age: 21}
+// age: 21
+// name: "Vieri"
+// __proto__: Object
