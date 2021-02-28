7d87daf3706709243f88e0d0b6dd2c3a9251d29b
viieee <adhityaharviando@gmail.com>
2021-02-28 22:45:16 +0700

    prototypes

--- Udemy/modul 11/prototypes-01-starting-setup/app.js
+++ Udemy/modul 11/prototypes-01-starting-setup/app.js
@@ -1,19 +1,19 @@
-// class ParentClass{
-//     method1(){
+class ParentClass{
+    method1(){
 
-//     }
-// }
- class Person {//extends ParentClass{
-  
-     constructor(){
-         // super();
-         this.name='Vieri';
-         this.age=21;
-      }
-      greet(){
-          console.log('Hi, my name is '+ this.name + ', i\'m '+this.age);
-      }
- }
+    }
+}
+class Person extends ParentClass{
+
+  constructor() {
+    super();
+    this.name = 'Vieri';
+    this.age = 21;
+  }
+  greet() {
+    console.log('Hi, my name is ' + this.name + ", i'm " + this.age);
+  }
+}
 
 // function Person(){
 //     this.age = 30;
@@ -23,14 +23,12 @@
 //     }
 // }
 
-//assigning new prototype/fallback object into objects that will be created with constructor function 
-Person.prototype.method2 = function(){
-
-}
+//assigning new prototype/fallback object into objects that will be created with constructor function
+Person.prototype.method2 = function () {};
 console.dir(Person);
 
-const prsn = new Person(); 
+const prsn = new Person();
 prsn.greet();
-console.log(prsn)
+console.log(prsn);
 console.log(new prsn.__proto__.constructor()); // you can make new object like this based on existing object and not a class / constructor function
 // console.dir(ParentClass);
