9494779416b1640aab8508c82c156e1e8bb0baf2
viieee <adhityaharviando@gmail.com>
2021-02-28 18:29:45 +0700

    proto

--- Udemy/modul 11/prototypes-01-starting-setup/app.js
+++ Udemy/modul 11/prototypes-01-starting-setup/app.js
@@ -3,35 +3,34 @@
 
 //     }
 // }
-// class Person {//extends ParentClass{
+ class Person {//extends ParentClass{
   
-//     constructor(){
-//         // super();
-//         this.name='Vieri';
-//         this.age=21;
-//      }
-//      greet(){
-//          console.log('Hi, my name is '+ this.name + ', i\'m '+this.age);
-//      }
-//  }
+     constructor(){
+         // super();
+         this.name='Vieri';
+         this.age=21;
+      }
+      greet(){
+          console.log('Hi, my name is '+ this.name + ', i\'m '+this.age);
+      }
+ }
 
- function Person(){
-         this.age = 30;
-         this.name = 'Vieri';
-         this.greet = ()=>{
-             console.log('Hi, my name is '+ this.name + ', i\'m '+this.age);
-         }
+// function Person(){
+//     this.age = 30;
+//     this.name = 'Vieri';
+//     this.greet = ()=>{
+//         console.log('Hi, my name is '+ this.name + ', i\'m '+this.age);
+//     }
+// }
+
+//assigning new prototype/fallback object into objects that will be created with constructor function 
+Person.prototype.method2 = function(){
+
 }
-
-// only applies to constructor function
-Person.prototype = { 
-    method2(){
-
-    }
-};
 console.dir(Person);
 
 const prsn = new Person(); 
 prsn.greet();
 console.log(prsn)
+console.log(new prsn.__proto__.constructor()); // you can make new object like this based on existing object and not a class / constructor function
 // console.dir(ParentClass);
