8d67ecf6a9bbc6cf553fb75fe280eb12d89cc13b
Viieee <adhityaharviando@gmail.com>
2021-03-02 19:20:10 +0700

    scoping

--- Udemy/modul 11/prototypes-01-starting-setup/app.js
+++ Udemy/modul 11/prototypes-01-starting-setup/app.js
@@ -1,30 +1,32 @@
-class ParentClass{
-  constructor(){
-    this.parent = 'parent';
-  }
-    method1(){
+// class ParentClass{
+//   constructor(){
+//     this.parent = 'parent';
+//   }
+//     method1(){
 
-    }
-}
-class Person extends ParentClass{
-
-  constructor() {
-    super();
-    this.name = 'Vieri';
-    this.age = 21;
-  }
-  greet() {
-    console.log('Hi, my name is ' + this.name + ", i'm " + this.age);
-  }
-}
-
-// function Person(){
-//     this.age = 30;
-//     this.name = 'Vieri';
-//     this.greet = ()=>{
-//         console.log('Hi, my name is '+ this.name + ', i\'m '+this.age);
 //     }
 // }
+// class Person extends ParentClass{
+
+//   constructor() {
+//     super();
+//     this.name = 'Vieri';
+//     this.age = 21;
+//   }
+//   greet() {
+//     console.log('Hi, my name is ' + this.name + ", i'm " + this.age);
+//   }
+// }
+
+function Person(){
+    this.age = 30;
+    this.name = 'Vieri';
+    
+  }
+  
+Person.prototype.greet = function(){
+  console.log('Hi, my name is '+ this.name + ', i\'m '+this.age);    
+}
 
 //assigning new prototype/fallback object into objects that will be created with constructor function
 Person.prototype.method2 = function () {};
