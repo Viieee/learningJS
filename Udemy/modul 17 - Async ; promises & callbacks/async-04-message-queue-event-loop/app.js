ff723f1714ab9897dc33295bfd78fa714d9b27fa
Viieee <adhityaharviando@gmail.com>
2021-04-09 18:19:09 +0700

    sync await

--- Udemy/modul 17 - Async ; promises & callbacks/async-04-message-queue-event-loop/app.js
+++ Udemy/modul 17 - Async ; promises & callbacks/async-04-message-queue-event-loop/app.js
@@ -25,60 +25,72 @@ function setTimer(duration){
   return promise;
 }
 
-function trackUserHandler() {
-  // without promise
-  // navigator.geolocation.getCurrentPosition(
-  //   posData => {
-  //     // setTimeout(() => {
-  //     //   console.log(posData);
-  //     // }, 2000);
-  //     setTimer(2000).then(data=>{
-  //       console.log(data, posData); // data is the text passed on to our promised above
-  //     })
-  //   },
-  //   error => {
-  //     console.log(error);
-  //   }
-  // );
-  // with promise
-  let positionData;
-  getPos()
-  .then(posData => {
-    // this code will run if getPos() method is resolved
-    positionData = posData;
-    // posData passed into this method from the resolved function in the promise function
-    // we use position data (a varaible inside this trackuserHandler function) to pass around data into 
-    // chained 'then' method
-    return setTimer(2000);
-    // if we do this (returning a promise)
-    // the setTimer promise function will execute once the code in this 'then' method is done
-    // if we passing something other than a promise (e.g string)
-    // the data in the then chained after this will reflect the string we returned
-  }//, error =>{
-    //console.log(error);}
-    // is the same as using .catch()
-  ).then(data=>{
-    // this will be executed once the setTimer function is done
-    // this behaviour is caused by us returning a promise in previous then method
-    console.log(data, positionData);
-  }).catch(error=>{
-    console.log(error);
-    // this catch method will run if any promise prior to this catch is rejected
-    // in this case if i block location permission on browser,
-    // the then method after getPos() and before this catch method won't run and skipped to this catch method
+// function trackUserHandler() {
+//   // without promise
+//   // navigator.geolocation.getCurrentPosition(
+//   //   posData => {
+//   //     // setTimeout(() => {
+//   //     //   console.log(posData);
+//   //     // }, 2000);
+//   //     setTimer(2000).then(data=>{
+//   //       console.log(data, posData); // data is the text passed on to our promised above
+//   //     })
+//   //   },
+//   //   error => {
+//   //     console.log(error);
+//   //   }
+//   // );
+//   // with promise
+//   let positionData;
+//   getPos()
+//   .then(posData => {
+//     // this code will run if getPos() method is resolved
+//     positionData = posData;
+//     // posData passed into this method from the resolved function in the promise function
+//     // we use position data (a varaible inside this trackuserHandler function) to pass around data into 
+//     // chained 'then' method
+//     return setTimer(2000);
+//     // if we do this (returning a promise)
+//     // the setTimer promise function will execute once the code in this 'then' method is done
+//     // if we passing something other than a promise (e.g string)
+//     // the data in the then chained after this will reflect the string we returned
+//   }//, error =>{
+//     //console.log(error);}
+//     // is the same as using .catch()
+//   ).then(data=>{
+//     // this will be executed once the setTimer function is done
+//     // this behaviour is caused by us returning a promise in previous then method
+//     console.log(data, positionData);
+//   }).catch(error=>{
+//     console.log(error);
+//     // this catch method will run if any promise prior to this catch is rejected
+//     // in this case if i block location permission on browser,
+//     // the then method after getPos() and before this catch method won't run and skipped to this catch method
     
-    // return setTimer(1000);
-    // if you returning something here
-    // the then after this catch method will be executed based on the returned object
-  });
+//     // return setTimer(1000);
+//     // if you returning something here
+//     // the then after this catch method will be executed based on the returned object
+//   });
 
-  // setTimeout(() => {
-  //   console.log('Timer done!');
-  // }, 0);
-  setTimer(1000).then(()=>{
-    console.log('Timer Done!')
-  })
-  console.log('Getting position...');
+//   // setTimeout(() => {
+//   //   console.log('Timer done!');
+//   // }, 0);
+//   setTimer(1000).then(()=>{
+//     console.log('Timer Done!')
+//   })
+//   console.log('Getting position...');
+// }
+
+async function trackUserHandler() {
+  let posData;
+  let timerData;
+  try{
+    posData = await getPos();
+    timerData = await setTimer(2000);
+  }catch(error){
+    console.log(error);
+  }
+  console.log(timerData, posData);
 }
 
 button.addEventListener('click', trackUserHandler);
