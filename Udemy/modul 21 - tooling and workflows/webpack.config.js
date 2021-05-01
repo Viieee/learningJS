7e2ca48c8f7e0aed9b51da180d7dc69b3961f353
viieee <adhityaharviando@gmail.com>
2021-05-01 23:53:06 +0700

    webpack

--- Udemy/modul 21 - tooling and workflows/webpack.config.js
+++ Udemy/modul 21 - tooling and workflows/webpack.config.js
@@ -7,11 +7,16 @@ const path = require('path');
 // exposing the object in node js outside of the file and webpack
 module.exports = {
     // entry file
-    mode: 'development',
+    mode: 'development', // the default value of this mode property is 'production'
+                         // if it is in the production mode, webpack will 
+                         // implement some optimization for the bundle 
     entry: './events-01-starting-setup/src/app.js',
     output: { // this is the place were the bundled file will be stored
         filename: 'app.js',
         path: path.resolve(__dirname, 'events-01-starting-setup', 'assets', 'scripts'), // same as ./assets/scripts/
         publicPath: 'events-01-starting-setup/assets/scripts/'
+        
+        // publicPath is being used as a map to tell the webpack
+        // where the extra files needed to be load
     }
 };
