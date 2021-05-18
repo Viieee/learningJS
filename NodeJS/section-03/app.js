08d2df731b15f18f496d1f3163dc6251dee772b6
Viieee <adhityaharviando@gmail.com>
2021-05-18 16:59:24 +0700

    notes

---
+++ NodeJS/section-03/app.js
@@ -0,0 +1,18 @@
+// create a server through nodejs
+
+// importing a module
+const http = require('http'); // with this method you can also import a js file
+          
+// request listener, the createServer argument
+function rqListener(req, res){
+    // this function takes 2 arguments, 
+    // 1st argument represent the incoming request
+    // 2nd argument represent the response that can be returned
+    console.log(req);
+}
+
+// create server
+const server = http.createServer(rqListener) // the argument of this method is a function that will be executed
+                                            // for every incoming request
+
+server.listen();
