89f0ae566447f3290fa85d9754180e335a5d2177
Viieee <adhityaharviando@gmail.com>
2021-05-21 13:49:20 +0700

    body parser

--- NodeJS/section-05/app.js
+++ NodeJS/section-05/app.js
@@ -1,19 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// parsing body of the request sent through a form
+// parser using body-parser
+// npm install --save body-parser
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+// routing
+// this will always execute as long as we dont send it
+// applied to all request
 app.use('/', (req, res, next) => {
-    console.log('This always runs!');
-    next();
+  next();
 });
 
+// trigger for request that goes to 'url/add-product'
 app.use('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<h1>The "Add Product" Page</h1>');
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
+// trigger for request that goes to 'url/product'
+app.use('/product', (req,res,next) => {
+  console.log(req.body);
+  // redirecting
+  res.redirect('/') // redirecting into 'url/'
+}) 
+
+// trigger for request that goes to 'url/'
 app.use('/', (req, res, next) => {
-  console.log('In another middleware!');
   res.send('<h1>Hello from Express!</h1>');
 });
 
