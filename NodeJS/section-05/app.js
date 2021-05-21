70802b0c14fd6b80e05b59a11ac0e522278b0928
Viieee <adhityaharviando@gmail.com>
2021-05-21 14:41:17 +0700

    new new

--- NodeJS/section-05/app.js
+++ NodeJS/section-05/app.js
@@ -3,34 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// parsing body of the request sent through a form
-// parser using body-parser
-// npm install --save body-parser
-app.use(bodyParser.urlencoded({extended:true}));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
+app.use(bodyParser.urlencoded({extended: false}));
 
-// routing
-// this will always execute as long as we dont send it
-// applied to all request
-app.use('/', (req, res, next) => {
-  next();
-});
-
-// trigger for request that goes to 'url/add-product'
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-});
-
-// trigger for request that goes to 'url/product'
-app.use('/product', (req,res,next) => {
-  console.log(req.body);
-  // redirecting
-  res.redirect('/') // redirecting into 'url/'
-}) 
-
-// trigger for request that goes to 'url/'
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
