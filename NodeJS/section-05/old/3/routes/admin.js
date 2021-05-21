4042ae97427aa87bbc4556c8dc5b7d89a0a66889
Viieee <adhityaharviando@gmail.com>
2021-05-21 15:43:16 +0700

    new new

--- NodeJS/section-05/old/3/routes/admin.js
+++ NodeJS/section-05/old/3/routes/admin.js
@@ -1,18 +1,20 @@
-const path = require('path');
-
 const express = require('express');
 
+// router
 const router = express.Router();
 
-// /admin/add-product => GET
+// admin/add-product => GET request
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.send(
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  );
 });
 
-// /admin/add-product => POST
+// admin/add-product => POST request
 router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
 
+// exporting
 module.exports = router;
