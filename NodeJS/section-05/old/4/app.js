8c090f11f8904253f006549475a33f20bc7a3912
Viieee <adhityaharviando@gmail.com>
2021-05-21 18:57:31 +0700

    new new

--- NodeJS/section-05/old/4/app.js
+++ NodeJS/section-05/old/4/app.js
@@ -9,12 +9,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
+// false route acessed
 app.use((req, res, next) => {
+    // sending 404 error page file to the user
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
--- NodeJS/section-05/old/4/routes/shop.js
+++ NodeJS/section-05/old/4/routes/shop.js
@@ -2,12 +2,11 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // sending a file to the user
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
