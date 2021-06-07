1290d8af4492ce8371cddbf9eab38812a070f407
Viieee <adhityaharviando@gmail.com>
2021-06-07 21:54:30 +0700

    preparation review code tomorrow

--- NodeJS/section-10; sql introduction/util/database.js
+++ NodeJS/section-10; sql introduction/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('node-complete', 'root', 'pass123.', {
   dialect: 'mysql',
   host: 'localhost'
 });
