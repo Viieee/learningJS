530e969b6aa2b1697abd60d488add9d9670663f7
Viieee <adhityaharviando@gmail.com>
2021-06-07 15:27:27 +0700

    creating dummy user

--- NodeJS/section-10; sql introduction/app.js
+++ NodeJS/section-10; sql introduction/app.js
@@ -6,6 +6,10 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 // importing the sequelize
 const sequelize = require('./util/database');
+// importing the models 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 
 const app = express();
 
@@ -23,10 +27,32 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// relating models
+    // a user created the product
+Product.belongsTo(User, {
+    constraints: true,
+    onDelete: 'CASCADE'
+}); 
+    // or and
+User.hasMany(Product);
+
 // lookin at all the models defined and creates tables for them
 sequelize.sync()
 .then(result=>{
     // listening to the result / what we get as a response
+    return User.findByPk(1);
+})
+.then(user=>{
+    if(!user){
+        return User.create({
+            name: 'Vieri',
+            email: "dummyEmail@gmail.com"
+        })
+    }
+    return Promise.resolve(user);
+})
+.then(user => {
+    console.log(user)
     app.listen(3000);
 })
 .catch(err=>{
