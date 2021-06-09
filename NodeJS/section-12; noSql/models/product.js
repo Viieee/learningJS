1b22b1e69baeddc8b7f27b02d27fb98326cdf5d0
Viieee <adhityaharviando@gmail.com>
2021-06-09 20:40:02 +0700

    using mongo

--- NodeJS/section-12; noSql/models/product.js
+++ NodeJS/section-12; noSql/models/product.js
@@ -1,27 +1,35 @@
-const Sequelize = require('sequelize');
+// importing access to the database
+const getDb = require('../util/database').getDb;
 
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product {
+  constructor(title, price, imageUrl, description){
+    this.title = title
+    this.price = price
+    this.imageUrl = imageUrl
+    this.description = description
   }
-});
+
+  // saving the object created with this class to database
+  save(){
+    // get access to the database
+    const db = getDb();
+    // telling mongoDb which collection you want to work with
+    // if its not exist then it will be created
+    /*products collection*/
+    // you can insert data with insertOne() or insertMany()
+    // insertOne() will take an object 
+    // db.collection('products').insertOne({name: 'A Book', price: 99})
+    // insertMany() will take an array of objects
+
+    // in this case we want to insert the object we created through this class
+    db.collection('products').insertOne(this)
+    .then(result=>{
+      console.log(result)
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+}
 
 module.exports = Product;
--- NodeJS/section-12; noSql/util/database.js
+++ NodeJS/section-12; noSql/util/database.js
@@ -1,8 +1,35 @@
-const Sequelize = require('sequelize');
+// importing mongoDb
+// npm install --save mongodb
+const mongoDb = require('mongodb');
+// accessing mongo client constructor
+const MongoClient = mongoDb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'pass123.', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+// using the client to connect the app to the mongoDb database
+const mongoConnect = (callback) =>{
+  MongoClient.connect('mongodb+srv://vie:pass123.@cluster0.kbsee.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(result=>{
+    console.log('connected!')
+    // storing the connection to the database
+    _db = result.db()
+    callback();
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+}
+
+const getDb = () =>{
+  if(_db){
+    // if db is set
+    // we returning the connection to the database
+    return _db;
+  }
+  throw 'no database found!';
+}
+ 
+
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb
