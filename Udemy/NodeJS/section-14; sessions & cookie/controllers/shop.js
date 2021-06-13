8149613cae496358591aeec0f8d2ed1afe477922
Viieee <adhityaharviando@gmail.com>
2021-06-13 11:53:17 +0700

    new new

--- Udemy/NodeJS/section-14; sessions & cookie/controllers/shop.js
+++ Udemy/NodeJS/section-14; sessions & cookie/controllers/shop.js
@@ -1,33 +1,10 @@
-// importing the models
 const Product = require('../models/product');
 const Order = require('../models/order');
-const user = require('../models/user');
 
-// exporting the getIndex method 
-// this method will execute when we accessing the default url /
-// we want to fetch all the data in product database and passing it into the ejs file
-exports.getIndex = (req, res, next) => {
-  Product.find() // getting all products (it's an array of objects)
-    .then(products => {
-      res.render('shop/index', {
-        prods: products, // its an array
-        pageTitle: 'Shop',
-        path: '/'
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-};
-
-
-// exporting getProducts method
-// this method will execute when we accessing /products url
-// the main purpose of this method is to fetch all data in product table
-  // and passing all the data into the ejs file
 exports.getProducts = (req, res, next) => {
-  Product.find() // getting all products (it's an array of objects)
+  Product.find()
     .then(products => {
+      console.log(products);
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
@@ -39,23 +16,12 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-// exporting getProduct method
-// this method will execute when we accessing /products/:productId url
-// the main purpose of this method is to fetch detailed information on one product
 exports.getProduct = (req, res, next) => {
-  // fetching the product's id
-    // the id is passed on to this method through url in href attribute in 'a' tag
   const prodId = req.params.productId;
-
-  // searching the product with the id of prodId
-  // the returned value will be passed onto the then method
-  // findById is a method defined by mongoose
   Product.findById(prodId)
     .then(product => {
-      // after we found the product by id 
-      // we passing in the returned value into the ejs file
       res.render('shop/product-detail', {
-        product: product, // its an object
+        product: product,
         pageTitle: product.title,
         path: '/products'
       });
@@ -63,119 +29,86 @@ exports.getProduct = (req, res, next) => {
     .catch(err => console.log(err));
 };
 
-// exporting the getCart method
-// this method will execute when we accessing the /cart url
-// the main purpose of this method is to fetch all products in the cart of user
-// this method is based on each user
-exports.getCart = (req, res, next) => {
-  // we're using req.user because with it we can access all data related to user
+exports.getIndex = (req, res, next) => {
+  Product.find()
+    .then(products => {
+      res.render('shop/index', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/'
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
-  // getting the cart of user
+exports.getCart = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
-    .execPopulate() // making it a promise
-    .then(
-      // we are getting back user data with products populated in it
-      user => {
-      console.log(user.cart.items)
-      // items is an array of product objects
-      // the product objects contains the id and the quantity by default
-      // but because we use populate method, now all the details of the product is also exist
-      const products = user.cart.items
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
-        products: products // its an array of product objects
+        products: products
       });
     })
     .catch(err => console.log(err));
 };
 
-// exporting the postCart method
-// this method will execute when we adding a product to the cart
 exports.postCart = (req, res, next) => {
-  // fetching product id from hidden input tag in the ejs file
   const prodId = req.body.productId;
-
   Product.findById(prodId)
-  .then(product=>{
-    // addToCart is the method in the user model
-    return req.user.addToCart(product);
-  })
-  .then(result=>{
-    console.log(result)
-    res.redirect('/cart')
-  })
-  .catch(err=>{
-    console.log(err)
-  })
+    .then(product => {
+      return req.user.addToCart(product);
+    })
+    .then(result => {
+      console.log(result);
+      res.redirect('/cart');
+    });
 };
 
-// exporting the postCartDeleteProduct
-// this method will execute when we are deleting a product in cart
 exports.postCartDeleteProduct = (req, res, next) => {
-  // getting the product's id from the hidden input tag in the ejs file
   const prodId = req.body.productId;
-  req.user.deleteItemFromCart(prodId)
+  req.user
+    .removeFromCart(prodId)
     .then(result => {
-      // after all the operations in the previous then methods is done
-      // we redirecting to cart page
       res.redirect('/cart');
     })
     .catch(err => console.log(err));
 };
 
-// exporting the postOrder method
-// this method will trigger when we want to move items from cart to order
 exports.postOrder = (req, res, next) => {
-  // fetching the products in user's cart
   req.user
-  .populate('cart.items.productId')
-  .execPopulate() // making it a promise
-  .then(
-    // we are getting back user data with products populated in it
-    user => {
-    console.log(user.cart.items)
-    // items is an array of product objects
-    // the product objects contains the id and the quantity by default
-    // but because we use populate method, now all the details of the product is also exist
-    const products = user.cart.items.map(i=>{
-      // we want to execute this for every product inside the cart
-      // we want to fill this products constant with objects 
-      // and the objects has product (the details) and quantity as the key
-      // {...i.productId._doc} will pull all the data about the product and storing it in product key
-      return {quantity: i.quantity, product: {...i.productId._doc}}
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          name: req.user.name,
+          userId: req.user
+        },
+        products: products
+      });
+      return order.save();
     })
-     // making the order object
-    const order = new Order({
-      products: products, // referring to the products constant
-      user:{
-        name: req.user.name,
-        userId: req.user._id
-      }
-    });
-    // saving the order
-    return order.save()
-  })
-  .then(result => {
-    // then after all operations is done
-    // we redirect to /orders
-    return req.user.clearCart()
-  })
-  .then(()=>{
-    res.redirect('/orders');
-  })
-  .catch(err => console.log(err));
+    .then(result => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect('/orders');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  // getting user's order
-  Order
-    .find({'user.userId': req.user._id})
+  Order.find({ 'user.userId': req.user._id })
     .then(orders => {
-      // because we including the product table in the getOrders method
-      // we're getting all the data in orderItems table and product table as extra data with it
-      // orders is an array
       res.render('shop/orders', {
         path: '/orders',
         pageTitle: 'Your Orders',
--- Udemy/NodeJS/section-14; sessions & cookie/models/user.js
+++ Udemy/NodeJS/section-14; sessions & cookie/models/user.js
@@ -1,101 +1,193 @@
-// importing mongoose 
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-// schema object from mongoose
 const Schema = mongoose.Schema;
 
-// making new schema
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    // cart embeeded document in the user
-    cart: {
-        // array of documents
-        items: [{
-            productId: {
-                // telling mongoose this field will store an object id
-                type: Schema.Types.ObjectId,
-                ref: 'Product',
-                required: true
-            },
-            quantity: {
-                type: Number,
-                required: true
-            }
-        }] 
-    }   
-})
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  cart: {
+    items: [
+      {
+        productId: {
+          type: Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true
+        },
+        quantity: { type: Number, required: true }
+      }
+    ]
+  }
+});
 
-// adding your own mehtod
-userSchema.methods.addToCart = function(product){
-    // finding whether the product is already in the cart or not
-    const cartProductIndex = this.cart.items.findIndex(cp=>{ // items array in the cart
-                                                        // cp is the individual product in the array
-      // executing for every element in the items array
-      // we want to return true if we find the product in the items array
-      return cp.productId.toString() === product._id.toString()
-    })
+userSchema.methods.addToCart = function(product) {
+  const cartProductIndex = this.cart.items.findIndex(cp => {
+    return cp.productId.toString() === product._id.toString();
+  });
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
 
-    // default value
-    let newQuantity = 1;
-    // copying all the data in the items array
-    const updatedCartItems = [...this.cart.items];
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  } else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity
+    });
+  }
+  const updatedCart = {
+    items: updatedCartItems
+  };
+  this.cart = updatedCart;
+  return this.save();
+};
 
-    if(cartProductIndex >= 0){
-      // if the item already exist we adding the quantity by 1
-      newQuantity = this.cart.items[cartProductIndex].quantity + 1 ;
-      updatedCartItems[cartProductIndex].quantity = newQuantity;
-    }else{
-      // if the item is new to cart
-      // we adding a new one to the array
-      updatedCartItems.push(
-        {
-          // we only want to store product's id
-          // and the quantity of the product in cart
-          productId: product._id, 
-          quantity: newQuantity
-        }
-      )
-    }
+userSchema.methods.removeFromCart = function(productId) {
+  const updatedCartItems = this.cart.items.filter(item => {
+    return item.productId.toString() !== productId.toString();
+  });
+  this.cart.items = updatedCartItems;
+  return this.save();
+};
 
-    // the cart object
-    // the cart object will have 1 key called items
-    // items will store an array of object
-    // and in the object it will store all the products and the quantity inside of the cart
-    const updatedCart = {
-      items: updatedCartItems
-    }
+userSchema.methods.clearCart = function() {
+  this.cart = { items: [] };
+  return this.save();
+};
 
-    this.cart = updatedCart
+module.exports = mongoose.model('User', userSchema);
 
-    // saving the cart
-    return this.save();
-}
+// const mongodb = require('mongodb');
+// const getDb = require('../util/database').getDb;
 
-userSchema.methods.deleteItemFromCart = function(productId){
-    // filtering the cart items, only getting back all the items that dont have 
-        // the id the same as id(the argument of this method)
-    const updatedCartItems = this.cart.items.filter(item=>{
-        // keeping all the product that dont match the id passed onto this method
-        return item.productId.toString() !== productId.toString();
-    })
-    // altering the value of the items array
-    this.cart.items = updatedCartItems;
-    // saving the change
-    return this.save()
-}
+// const ObjectId = mongodb.ObjectId;
 
+// class User {
+//   constructor(username, email, cart, id) {
+//     this.name = username;
+//     this.email = email;
+//     this.cart = cart; // {items: []}
+//     this._id = id;
+//   }
 
-userSchema.methods.clearCart = function(){
-    this.cart = {items:[]}
-    return this.save();
-}
+//   save() {
+//     const db = getDb();
+//     return db.collection('users').insertOne(this);
+//   }
 
-// exporting the mongoose model
-module.exports = mongoose.model('User', userSchema)
+//   addToCart(product) {
+//     const cartProductIndex = this.cart.items.findIndex(cp => {
+//       return cp.productId.toString() === product._id.toString();
+//     });
+//     let newQuantity = 1;
+//     const updatedCartItems = [...this.cart.items];
+
+//     if (cartProductIndex >= 0) {
+//       newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+//       updatedCartItems[cartProductIndex].quantity = newQuantity;
+//     } else {
+//       updatedCartItems.push({
+//         productId: new ObjectId(product._id),
+//         quantity: newQuantity
+//       });
+//     }
+//     const updatedCart = {
+//       items: updatedCartItems
+//     };
+//     const db = getDb();
+//     return db
+//       .collection('users')
+//       .updateOne(
+//         { _id: new ObjectId(this._id) },
+//         { $set: { cart: updatedCart } }
+//       );
+//   }
+
+//   getCart() {
+//     const db = getDb();
+//     const productIds = this.cart.items.map(i => {
+//       return i.productId;
+//     });
+//     return db
+//       .collection('products')
+//       .find({ _id: { $in: productIds } })
+//       .toArray()
+//       .then(products => {
+//         return products.map(p => {
+//           return {
+//             ...p,
+//             quantity: this.cart.items.find(i => {
+//               return i.productId.toString() === p._id.toString();
+//             }).quantity
+//           };
+//         });
+//       });
+//   }
+
+//   deleteItemFromCart(productId) {
+//     const updatedCartItems = this.cart.items.filter(item => {
+//       return item.productId.toString() !== productId.toString();
+//     });
+//     const db = getDb();
+//     return db
+//       .collection('users')
+//       .updateOne(
+//         { _id: new ObjectId(this._id) },
+//         { $set: { cart: { items: updatedCartItems } } }
+//       );
+//   }
+
+//   addOrder() {
+//     const db = getDb();
+//     return this.getCart()
+//       .then(products => {
+//         const order = {
+//           items: products,
+//           user: {
+//             _id: new ObjectId(this._id),
+//             name: this.name
+//           }
+//         };
+//         return db.collection('orders').insertOne(order);
+//       })
+//       .then(result => {
+//         this.cart = { items: [] };
+//         return db
+//           .collection('users')
+//           .updateOne(
+//             { _id: new ObjectId(this._id) },
+//             { $set: { cart: { items: [] } } }
+//           );
+//       });
+//   }
+
+//   getOrders() {
+//     const db = getDb();
+//     return db
+//       .collection('orders')
+//       .find({ 'user._id': new ObjectId(this._id) })
+//       .toArray();
+//   }
+
+//   static findById(userId) {
+//     const db = getDb();
+//     return db
+//       .collection('users')
+//       .findOne({ _id: new ObjectId(userId) })
+//       .then(user => {
+//         console.log(user);
+//         return user;
+//       })
+//       .catch(err => {
+//         console.log(err);
+//       });
+//   }
+// }
+
+// module.exports = User;
