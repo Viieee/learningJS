52dd1a158901c2e72764af8058ea8fac9e0b8fa7
Viieee <adhityaharviando@gmail.com>
2021-06-14 14:21:38 +0700

    donee

--- Udemy/NodeJS/section-14; sessions & cookie/app.js
+++ Udemy/NodeJS/section-14; sessions & cookie/app.js
@@ -53,6 +53,19 @@ app.use(
   })
 );
 
+app.use((req,res,next)=>{
+  if(!req.session.user){
+    return next();
+  }
+  User.findById(req.session.user._id) // finding user id based on the current session
+    .then(user => {
+      // req.user contain full user object with all the methods based on current session
+      req.user = user
+      next();
+    })
+    .catch(err => console.log(err));
+})
+
 // using the routes
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
