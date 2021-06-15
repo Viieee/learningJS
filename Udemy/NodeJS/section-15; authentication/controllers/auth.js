f07f877ddcf5bd1073676ddf0fae0c26076a9b81
Viieee <adhityaharviando@gmail.com>
2021-06-15 16:32:11 +0700

    authentication basic

--- Udemy/NodeJS/section-15; authentication/controllers/auth.js
+++ Udemy/NodeJS/section-15; authentication/controllers/auth.js
@@ -17,19 +17,51 @@ exports.getSignup = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
-    .then(user => {
-      req.session.isLoggedIn = true;
-      req.session.user = user;
-      req.session.save(err => {
-        console.log(err);
-        res.redirect('/');
-      });
-    })
-    .catch(err => console.log(err));
+  User.findById('60c5395ba98da70520a7b8ee')
+  .then(user => {
+    req.session.isLoggedIn = true;
+    req.session.user = user;
+    req.session.save(err => {
+      console.log(err);
+      res.redirect('/');
+    });
+  })
+  .catch(err => console.log(err));
 };
 
-exports.postSignup = (req, res, next) => {};
+exports.postSignup = (req, res, next) => {
+  // retrieving data from form
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+
+  // finding if a user with the email in the request already exist in our database
+  User.findOne({email: email})
+  .then(userDoc=>{
+    // we getting the user data back with the email entered in form
+    if(userDoc){
+      // if a user with the same email already exist in the database
+      return res.redirect('/signup')
+    }
+    // if theres no user with the email entered
+    const user = new User({
+      // user schema, look at the model
+      email: email,
+      password: password,
+      cart:{
+        items: []
+      }
+    })
+    // saving the user
+    return user.save()
+  })
+  .then(result=>{
+    res.redirect('/login')
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+};
 
 exports.postLogout = (req, res, next) => {
   req.session.destroy(err => {
